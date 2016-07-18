83405208e9a067766216e7fc7eb0cd868335b589
Matthew Mawson <matthewmawson@gmail.com>
2016-07-18 15:23:55 +0100

    Build 1.0.0

--- Delete Files/deleteFiles.ps1
+++ Delete Files/deleteFiles.ps1
@@ -1,3 +1,7 @@
-﻿#
-# Script.ps1
-#
+﻿param(
+	[Parameter(Mandatory = $true)] [string] $folder,
+	[Parameter(Mandatory = $true)] [int] $days,
+	[Parameter(Mandatory = $true)] [string] $filetype
+)
+
+Get-ChildItem –Path $folder* –Recurse -Include *.$filetype | Where-Object CreationTime –lt (Get-Date).AddDays($days) | Remove-Item
